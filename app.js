9199c7b411cb550504f476230e2241d6683d1f4f
ncmarsh <68823943+ncmarsh@users.noreply.github.com>
2020-11-06 18:59:33 -0800

    began inquirer prompts and functions for user questions

--- app.js
+++ app.js
@@ -9,10 +9,160 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { run } = require("jest");
 
+// Array of employees to add to
+const employees = [];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+// Initial questions to prompt the user
+const startQ = [
+    // different question path for the different people
+    // error if id number is already taken
+    {
+        prefix: "Let's build your team:",
+        type: "input",
+        message: "\nWhat is your manager's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your manager's id?",
+        name: "id",
+        // validate: function validateId(id) {
+        //     return id !== "";
+        // }
+    },
+    {
+        type: "input",
+        message: "What is your manager's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your manager's office number?",
+        name: "officeNumber"
+    }
+]
+
+const question = [
+    {
+        type: "list",
+        message: "Which type of team member would you like to add next?",
+        name: "role",
+        choices: [
+            "Engineer",
+            "Intern",
+            "I don't want to add any more team members."
+        ],
+        // when: function(end) {
+        //     return end.choices === "I don't want to add any more team members.";
+        // }
+    }
+]
+
+const engineerQ = [
+    {
+        type: "input",
+        message: "What is your engineer's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your engineer's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is your engineer's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your engineer's GitHub username?",
+        name: "github"
+    }
+]
+
+const internQ = [
+    {
+        type: "input",
+        message: "What is your intern's name?",
+        name: "name"
+    },
+    {
+        type: "input",
+        message: "What is your intern's id?",
+        name: "id"
+    },
+    {
+        type: "input",
+        message: "What is your intern's email?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is your intern's school?",
+        name: "school"
+    }
+]
+
+function runEngineer() {
+    inquirer
+        .prompt(
+            engineerQ
+        )
+        .then(function(response) {
+            employees.push(response);
+            console.log(employees);
+            nextRole();
+    })
+}
+
+function runIntern() {
+    inquirer
+        .prompt(
+            internQ
+        )
+        .then(function(response) {
+            employees.push(response);
+            console.log(employees);
+            nextRole();
+    })
+}
+
+function nextRole() {
+    inquirer
+        .prompt(
+            question
+        )
+        .then(function(response) {
+            console.log(response);
+            if (response.role === "Engineer") {
+                runEngineer();
+            }
+            else if (response.role === "Intern") {
+                runIntern();
+            }
+            else {
+                console.log("That's all folks");
+                console.log(employees);
+            }
+    })
+}
+
+inquirer
+    .prompt(
+        startQ
+    )
+    .then(function(response) {
+        console.log(response);
+            employees.push(response);
+            console.log(employees);
+            nextRole();
+    })
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +183,5 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+// Call the render function with the array of employees
